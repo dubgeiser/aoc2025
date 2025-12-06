23a364176f3cd8900ff9c672650acaff906060a7
Per Juchtmans <per@dubgeiser.net>
2025-12-06 13:10:35 +0100

    Day 3, part 2
    Got bitten by newline/string terminator :(

--- day03.c
+++ day03.c
@@ -1,23 +1,56 @@
 #include "lib/input.c"
+#include <stdint.h>
 #include <stdio.h>
 #include <string.h>
 
-int main() {
-  int p1 = 0;
-  char *line;
-  int max, n;
-  while (NULL != (line = readline(stdin))) {
-    max = 0;
-    for (size_t i = 0; i < strlen(line) - 1; i++) {
-      for (size_t j = i + 1; j < strlen(line); j++) {
-        n = 10 * (line[i] - '0') + (line[j] - '0');
-        if (n > max) {
-          max = n;
-        }
+int joltage1(char *line) {
+  int max = 0;
+  int n;
+  for (size_t i = 0; i < strlen(line) - 1; i++) {
+    for (size_t j = i + 1; j < strlen(line); j++) {
+      n = 10 * (line[i] - '0') + (line[j] - '0');
+      if (n > max) {
+        max = n;
       }
     }
-    p1 += max;
   }
-  printf("%d", p1);
+  return max;
+}
+
+int64_t joltage2(char *line) {
+  const int K = 12;
+  int idx = 0;
+  int pos = 0;
+  int L = strlen(line);
+  int64_t max = 0;
+  if (line[L - 1] == '\n') {
+    line[--L] = '\0';
+  }
+  while (idx < K) {
+    char maxd = '0';
+    int maxp = pos;
+    for (int i = pos; i <= L - (K - idx); i++) {
+      if (line[i] > maxd) {
+        maxd = line[i];
+        maxp = i;
+      }
+    }
+    max = max * 10 + (maxd - '0');
+    idx++;
+    pos = maxp + 1;
+  }
+  return max;
+}
+
+int main() {
+  int p1 = 0;
+  int64_t p2 = 0;
+  char *line;
+  while (NULL != (line = readline(stdin))) {
+    p1 += joltage1(line);
+    p2 += joltage2(line);
+  }
+  printf("%d\n", p1);
+  printf("%ld\n", p2);
   return 0;
 }
