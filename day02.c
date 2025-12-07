65b7a0039fdc213b66ec0154d2e3f3a44227a687
Per Juchtmans <per@dubgeiser.net>
2025-12-07 17:36:09 +0100

    Day 2, part 2

--- day02.c
+++ day02.c
@@ -51,8 +51,36 @@ int64_t sum_invalid_ids(Range r) {
   return sum;
 }
 
+int64_t sum_silly_ids(Range r) {
+  int64_t sum = 0;
+
+  for (int64_t n = r.start; n <= r.end; n++) {
+    char s[32];
+    snprintf(s, sizeof(s), "%lld", (long long)n);
+    int len = strlen(s);
+    for (int blk = 1; blk <= len / 2; blk++) {
+      if (len % blk != 0) {
+        continue;
+      }
+      int ok = 1;
+      for (int i = blk; i < len; i += blk) {
+        if (strncmp(s, s + i, blk) != 0) {
+          ok = 0;
+          break;
+        }
+      }
+      if (ok) {
+        sum += n;
+        break;
+      }
+    }
+  }
+  return sum;
+}
+
 int main() {
   int64_t p1 = 0;
+  int64_t p2 = 0;
   Range ranges[SIZE];
   char *token, *pstate;       // tokenizing on ","
   char *subtoken, *subpstate; // (sub)tokenizing on "-"
@@ -72,9 +100,11 @@ int main() {
 
   for (int i = 0; i < R; i++) {
     p1 += sum_invalid_ids(ranges[i]);
+    p2 += sum_silly_ids(ranges[i]);
   }
 
   printf("%ld\n", p1);
+  printf("%ld\n", p2);
 
   return 0;
 }
