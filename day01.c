40f839298b46660fae20e601a4fad89f3c6cd095
Per Juchtmans <per@dubgeiser.net>
2025-12-08 08:10:01 +0100

    Day 1, part 2
    ... Finally, this one had me chasing ghosts for a while...

--- day01.c
+++ day01.c
@@ -7,8 +7,21 @@ int diff(int pos, char d, int n) {
   return (pos + n) % 100;
 }
 
+int turn(int pos, char d, int n) {
+  int count = 0;
+  int inc = (d == 'L') ? -1 : 1;
+  for (int i = 0; i < n; i++) {
+    pos = (pos + inc + 100) % 100;
+    if (pos == 0) {
+      count++;
+    }
+  }
+  return count;
+}
+
 int main() {
   int p1 = 0;
+  int p2 = 0;
   char *line;
   int pos = 50;
 
@@ -17,6 +30,7 @@ int main() {
   while (NULL != (line = readline(stdin))) {
     sscanf(line, "%c%d", &dir, &amount);
     free(line);
+    p2 += turn(pos, dir, amount);
     pos = diff(pos, dir, amount);
     if (0 == pos) {
       p1++;
@@ -24,5 +38,6 @@ int main() {
   }
 
   printf("%d\n", p1);
+  printf("%d\n", p2);
   return 0;
 }
