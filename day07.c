df4be3095aa38a0f206df163cdc0c6a503200d6d
Per Juchtmans <per@dubgeiser.net>
2025-12-24 16:08:14 +0100

    Day 7, part 2
    Got bitten by the `unsigned int` vs `int64_t`, maybe I better make it a
    habit to always have `int64_t` in AoC :-)

--- day07.c
+++ day07.c
@@ -1,16 +1,24 @@
 #include "lib/input.c"
 
 #include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <string.h>
 
 #define SIZE 150
 
 typedef unsigned int uint;
+
 typedef struct {
   uint row;
   uint col;
 } Pos;
+
+typedef struct {
+  uint row;
+  uint col;
+  int64_t result;
+} Qpos;
 
 char grid[SIZE][SIZE];
 uint R = 0;
@@ -18,6 +26,9 @@ uint C = 0;
 
 Pos visited[SIZE * SIZE];
 uint V = 0;
+
+Qpos qcache[SIZE * SIZE];
+uint Q = 0;
 
 bool is_visited(uint row, uint col) {
   for (uint i = 0; i < V; i++) {
@@ -47,6 +58,44 @@ void downward(uint row, uint col) {
   }
 }
 
+int64_t cache_get(uint row, uint col) {
+  for (uint i = 0; i < Q; i++) {
+    if (row == qcache[i].row && col == qcache[i].col) {
+      return qcache[i].result;
+    }
+  }
+  return -1;
+}
+
+void cache_set(uint row, uint col, int64_t result) {
+  qcache[Q].row = row;
+  qcache[Q].col = col;
+  qcache[Q].result = result;
+  Q++;
+}
+
+int64_t qdownward(uint row, uint col) {
+  int64_t result = cache_get(row, col);
+  if (result > -1) {
+    return result;
+  }
+
+  int64_t timelines = 0;
+  for (uint i = row; i < R; i++) {
+    if (grid[i][col] == '.') {
+      continue;
+    }
+    result = qdownward(i, col - 1);
+    cache_set(i, col - 1, result);
+    timelines += result;
+    result = qdownward(i, col + 1);
+    cache_set(i, col + 1, result);
+    timelines += result;
+    return timelines;
+  }
+  return 1;
+}
+
 int main() {
   uint sr;
   uint sc;
@@ -68,5 +117,7 @@ int main() {
 
   downward(sr, sc);
   printf("%d\n", V);
+
+  printf("%ld\n", qdownward(sr, sc));
   return 0;
 }
