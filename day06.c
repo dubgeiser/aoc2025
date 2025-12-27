1a9c51c7edb2ace1efefabca99841cdfd2c9ac44
Per Juchtmans <per@dubgeiser.net>
2025-12-27 06:44:00 +0100

    Day 6, part 2
    Nasty, that different way of processing :-)
    A bit hairy re. the processing of the subtotal, but too tired to make it
    cleaner.

--- day06.c
+++ day06.c
@@ -2,6 +2,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define MAX_ROWS 5
 #define MAX_COLS 4000
@@ -41,7 +42,19 @@ int main() {
   int R = 0;
   int C = 0;
 
+  // Part 2
+  char grid[MAX_ROWS][MAX_COLS];
+  int gridW = 0;
+  int gridH = 0;
+
   while (NULL != (line = readline(stdin))) {
+    // Part 2
+    if (gridW == 0)
+      gridW = strlen(line);
+    for (int i = 0; i < gridW; i++)
+      grid[gridH][i] = line[i];
+    gridH++;
+
     if (line[0] == '+' || line[0] == '*') {
       consume_ops(line, ops);
     } else {
@@ -60,6 +73,45 @@ int main() {
     }
     p1 += tot;
   }
-
   printf("%ld\n", p1);
+
+  // Part 2
+  char cur;
+  int64_t p2 = 0;
+  for (int c = 0; c < gridW; c++) {
+    char snum[MAX_ROWS];
+    int snum_i = 0;
+    int64_t subtotal;
+    int64_t (*op)(int64_t, int64_t);
+    for (int r = 0; r < gridH; r++) {
+      cur = grid[r][c];
+      if (cur >= '0' && cur <= '9') {
+        snum[snum_i++] = cur;
+        continue;
+      }
+      // Only the last line matters for non-digits
+      if (r < gridH - 1)
+        continue;
+
+      // An empty column, add the subtotal to the total
+      if (cur == ' ' && snum_i == 0) {
+        p2 += subtotal;
+        continue;
+      }
+
+      snum[snum_i] = '\0';
+      int64_t n = atoll(snum);
+      if (cur == '+' || cur == '*') {
+        op = cur == '+' ? add : mul;
+        subtotal = n;
+      } else {
+        subtotal = op(subtotal, n);
+      }
+    }
+
+    // Last column, don't forget to add subtotal once more
+    if (c == gridW - 1)
+      p2 += subtotal;
+  }
+  printf("%ld\n", p2);
 }
